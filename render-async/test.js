a7ef1cdc47bd3c9faac0b124602721839faa6570
rhildred <rhildred@gmail.com>
2013-11-13 13:47:16 -0500

    commit stuff on stick so I can do a local pull

--- render-async/test.js
+++ render-async/test.js
@@ -4,9 +4,9 @@ renderFile = require('./render-async.js').__express;
 
 var tests = {
 	"Test hello": function(done){
-		renderFile(__dirname + '/examples/hello.js.html', {name: "Rich"}, function(err, html){
+		renderFile(__dirname + '/examples/hello.js.html', {first: "Rich", last: "Hildred"}, function(err, html){
 			assert(err == null);
-			assert(html == '<!DOCTYPE html><html lang="en"><body>hello Rich</body></html>');
+			assert(html == '<!DOCTYPE html><html lang="en"><body>hello Rich Hildred</body></html>');
 			done();
 		});
 		
