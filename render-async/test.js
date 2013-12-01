9eb09400db9710127151897ad4549ec762a5a2d2
Rich Hildred <rhildred@gmail.com>
2013-12-01 00:36:52 -0500

    complete render-async package ready to npm publish

--- render-async/test.js
+++ render-async/test.js
@@ -1,7 +1,12 @@
 var TestRunner = require('assert-runner'),
 assert = require('assert'),
-renderFile = require('./render-async.js').__express,
-express = require('express');
+renderAsync = require('./render-async.js'),
+renderFile = renderAsync.__express,
+jQuery = require('js-toolbox')._jQuery;
+
+//now we need a server for this so that we can test include
+var app= renderAsync.express({welcomeFile: "index.js.html"});
+app.set('views', __dirname + '/examples');
 
 var tests = {
 	"Test hello": function(done){
@@ -49,26 +54,146 @@ var tests = {
 			done();
 		});
 		
-	}
-};
+	},
+	"Test partial with Ajax": function(done){
+		renderFile(__dirname + '/examples/partialouter2.js.html', {}, function(err, html){
+			assert(err == null);
+			assert(html == '<ul><li>3</li><li>4</li></ul>');
+			done();
+		});
+		
+	},
+	"Test link with local resource": function(done){
+		renderFile(__dirname + '/examples/link.js.html', {}, function(err, html){
+			assert(err == null);
+			assert(html == '<script id="navTemplate" type="text/template"><li><%= test %></li></script>');
+			done();
+		});
+		
+	},
+	"Test link with external resource and no protocol": function(done){
+		renderFile(__dirname + '/examples/link2.js.html', {}, function(err, html){
+			assert(err == null);
+			assert(html == '<script> var oTest = [{"test": 3}, {"test": 4}];</script>');
+			done();
+		});
+		
+	},
+	"Test link with absolute resource": function(done){
+		renderFile(__dirname + '/examples/link3.js.html', {}, function(err, html){
+			assert(err == null);
+			assert(html == '<script> var oTest = [{"test": 3}, {"test": 4}];</script>');
+			done();
+		});
+		
+	},
+	"Test link with external resource with protocol": function(done){
+		renderFile(__dirname + '/examples/link4.js.html', {}, function(err, html){
+			assert(err == null);
+			assert(html == '<script> var oTest = [{"test": 3}, {"test": 4}];</script>');
+			done();
+		});
+		
+	},
+	"Install it and test with ejs extension": function(done){
+		app.engine('ejs', renderFile);
+		app.get("*.ejs", function(req, res){
+			res.render(req._parsedUrl.path.substring(1));
+		});
+		jQuery.ajax("http://localhost:8080/test.ejs")
+		.done(function(html){
+			assert(html == "Rich was here");
+			done();
+		})
+		.fail(function(err){
+			assert(err == false);
+		});
+	},
+	"Install it and test with js.html extension": function(done){
+		// we don't seem to be able to have an engine with a . in it.
+		app.get("*.js.html", renderAsync.renderAsync);
+		jQuery.ajax("http://localhost:8080/include.js.html")
+		.done(function(html){
+			assert(html == "<!Doctype html><body>included from _included.js.html <p>Rich was here</p></body></html>");
+			done();
+		})
+		.fail(function(err){
+			assert(err == false);
+		});
+	},
+	"Install it and test for welcome file": function(done){
+		// we don't seem to be able to have an engine with a . in it.
+		app.get("*.js.html", renderAsync.renderAsync);
+		jQuery.ajax("http://localhost:8080/")
+		.done(function(html){
+			assert(html == "<!Doctype html><body>Hello</body></html>");
+			done();
+		})
+		.fail(function(err){
+			assert(err == false);
+		});
+	},
+	"Make sure we still serve a plain old html file": function(done){
+		jQuery.ajax("http://localhost:8080/test.html")
+		.done(function(html){
+			assert(html == "hello html");
+			done();
+		})
+		.fail(function(err){
+			assert(err == false);
+		});
 
-// now we need a server for this so that we can test include
-var app=express();
+	},
+	"Try for French": function(done){
+		jQuery.ajax("http://localhost:8080/fr/multilingual.js.html")
+		.done(function(html){
+			assert(html == "Bonjour");
+			done();
+		})
+		.fail(function(err){
+			assert(err == false);
+		});
+
+	},
+	"Try for English": function(done){
+		jQuery.ajax("http://localhost:8080/multilingual.js.html")
+		.done(function(html){
+			assert(html == "Hello");
+			done();
+		})
+		.fail(function(err){
+			assert(err == false);
+		});
+
+	},
+	"Try for French in welcome file": function(done){
+		jQuery.ajax("http://localhost:8080/fr/")
+		.done(function(html){
+			assert(html == "<!Doctype html><body>Bonjour</body></html>");
+			done();
+		})
+		.fail(function(err){
+			assert(err == false);
+		});
+
+	},
+
+
+};
 
 app.get("/test", function(req, res){
     res.setHeader("Content-Type", "text/html");
     res.end("Hello World!");
 });
 
-//server everything index.html welcome file
-app.use(function(req, res){
-        res.sendfile(__dirname + req._parsedUrl.path, function(err){
-                if(err){
-                        console.log(err);
-                        res.send(err.status, err.code);
-                }
-        });
+app.get("/test2", function(req, res){
+    res.setHeader("Content-Type", "application/json");
+    res.end("[{\"test\": 3}, {\"test\": 4}]");
 });
+
+
+//server everything index.html welcome file
+app.use(renderAsync.webServer);
 
 
 //set ipaddress from openshift, to command line or to localhost:8080
